4f8f1747a2cf1148f11ca6d0a3d72ebe6d69e26e
Gexedd <eeviac.cl@gmail.com>
2023-02-23 10:06:10 -0300

    - Se hace el llamado a la API de AcuWeather por medio del paquete HTTP de FLutter.

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:clima/services/location.dart';
 
+import 'package:http/http.dart' as http;
+
 class LoadingScreen extends StatefulWidget {
   @override
   _LoadingScreenState createState() => _LoadingScreenState();
@@ -11,6 +13,7 @@ class _LoadingScreenState extends State<LoadingScreen> {
   void initState() {
     super.initState();
     getLocation();
+
   }
 
   //Creo el método
@@ -19,12 +22,27 @@ class _LoadingScreenState extends State<LoadingScreen> {
     Location location = Location();
     await location.getCurrentLocation();
 
-    print(location.latitude);
-    print(location.longitude);
+    //print(location.latitude);
+    //print(location.longitude);
+  }
+
+  //Creando un nuevo método para obtener la info del http
+  void getData() async {
+    http.Response response= await  http.get(Uri.parse('https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=0359c828acdd1faa944ac9e19b05fe3e'));
+
+  if (response.statusCode == 200) {
+    String data = response.body;
+    print(data);
+  }else {
+    print(response.statusCode);
+  }
+
   }
 
   @override
   Widget build(BuildContext context) {
+    getData();
     return Scaffold();
+
   }
 }
